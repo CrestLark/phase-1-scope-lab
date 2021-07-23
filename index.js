f9d0265a5e0cd7e93c75954e34a89643412956ae
crestlark <stephanie.lewis.art@gmail.com>
2021-07-22 19:17:56 -0600

    commit tree b8628ba78e2f9fb47a893ce2c73216290ae8f70f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = "bob";
+customerName();
+function upperCaseCustomerName() {
+    customerName =
+     customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer= "bobs wife";
+    leastFavoriteCustomer = "steves wife";
+}
